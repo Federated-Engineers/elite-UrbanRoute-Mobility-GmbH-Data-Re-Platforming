09031024ee185d08868a35f40217580cbf4769fb
Musili-Adebayo <adebayomusili@gmail.com>
2026-09-08 22:50:08 +0100

    new test

--- snowflake/production/external_tables.tf
+++ snowflake/production/external_tables.tf
@@ -1,10 +1,10 @@
 resource "snowflake_external_table" "trips_ext" {
   database     = snowflake_database.urban_route_db.name
   schema       = snowflake_schema.bronze.name
-  name         = "TRIPS"
+  name         = "TRIPS_TEST"
   comment      = "External table of trip JSON files stored in S3."
   location     = "@${snowflake_stage_external_s3.urban_route_stage.fully_qualified_name}"
-  file_format  = "FORMAT_NAME = ${snowflake_file_format.json_data.fully_qualified_name}"
+  file_format  = "FORMAT_NAME = ${snowflake_file_format.json_data.fullname}"
   auto_refresh = true
 
   column {
